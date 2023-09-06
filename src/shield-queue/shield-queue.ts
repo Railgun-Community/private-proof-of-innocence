b0d8198803b0eac13a93ad87ee71cb3c7f2ecdc7
jrmeurer <jrmeurer@wisc.edu>
2023-09-06 16:17:32 -0600

    Refactor timeAgo into its own file

--- src/shield-queue/shield-queue.ts
+++ src/shield-queue/shield-queue.ts
@@ -1,10 +1,7 @@
 import { NetworkName } from '@railgun-community/shared-models';
 import { ShieldQueueDatabase } from '../database/databases/shield-queue-database';
 import { ShieldStatus } from '../models/database-types';
-import TimeAgo from 'javascript-time-ago';
-
-import en from 'javascript-time-ago/locale/en';
-TimeAgo.addDefaultLocale(en);
+import { getFormattedTimeAgo } from '../util/time-ago';
 
 export const MAX_EVENT_QUERY_RANGE_LENGTH = 20;
 
@@ -26,11 +23,9 @@ export const getShieldQueueStatus = async (
   const blocked = await db.getCount(ShieldStatus.Blocked);
   const addedPoi = await db.getCount(ShieldStatus.AddedPOI);
 
-  const timeAgo = new TimeAgo('en-US');
-
   const latestPendingShield = await db.getLatestPendingShield();
   const latestPendingShieldTime = latestPendingShield
-    ? `${timeAgo.format(new Date(latestPendingShield.timestamp * 1000))}`
+    ? `${getFormattedTimeAgo(new Date(latestPendingShield.timestamp * 1000))}`
     : undefined;
 
   return {
