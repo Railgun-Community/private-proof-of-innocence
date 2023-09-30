36dc0b633f0b9946df337b355fe6187c761b0518
jrmeurer <jrmeurer@wisc.edu>
2023-09-30 12:56:51 -0600

    Up to 100 blinded commitments

--- packages/node/src/config/query-limits.ts
+++ packages/node/src/config/query-limits.ts
@@ -2,7 +2,7 @@ export class QueryLimits {
   // Max number of commitments for a single transaction
   static readonly GET_MERKLE_PROOFS_MAX_BLINDED_COMMITMENTS = 13;
 
-  static readonly GET_POI_EXISTENCE_MAX_BLINDED_COMMITMENTS = 30;
+  static readonly GET_POI_EXISTENCE_MAX_BLINDED_COMMITMENTS = 100;
 
   static readonly MAX_EVENT_QUERY_RANGE_LENGTH = 100;
 
