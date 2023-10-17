38ce45dd9ad2791198324fc0e390e2a4e3c2f37b
jrmeurer <jrmeurer@wisc.edu>
2023-10-17 06:48:44 -0600

    Increase limits for syncs

--- packages/node/src/config/query-limits.ts
+++ packages/node/src/config/query-limits.ts
@@ -4,9 +4,9 @@ export class QueryLimits {
 
   static readonly GET_POI_EXISTENCE_MAX_BLINDED_COMMITMENTS = 100;
 
-  static readonly MAX_EVENT_QUERY_RANGE_LENGTH = 100;
+  static readonly MAX_EVENT_QUERY_RANGE_LENGTH = 500;
 
-  static readonly PROOF_MEMPOOL_SYNCED_ITEMS = 100;
+  static readonly PROOF_MEMPOOL_SYNCED_ITEMS = 500;
 
   static readonly BLOCKED_SHIELDS_SYNCED_ITEMS = 50;
 }
