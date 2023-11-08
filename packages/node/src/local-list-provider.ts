2b8b8b57bdb1be693a8214ab7268c8325b56abd4
Pete S <ptaylorsimpson@gmail.com>
2023-11-08 00:43:52 -0800

    Removes ofac from test list

--- packages/node/src/local-list-provider.ts
+++ packages/node/src/local-list-provider.ts
@@ -15,8 +15,8 @@ const TEST_BAD_ADDRESS_LIST = [
 
 export class LocalListProvider extends ListProvider {
   protected config: ListProviderConfig = {
-    name: 'Chainalysis OFAC Sanctions List',
-    description: `Blocks OFAC-sanctioned addresses.`,
+    name: 'Test List',
+    description: `Blocks test addresses.`,
   };
 
   protected async shouldAllowShield(
@@ -28,51 +28,10 @@ export class LocalListProvider extends ListProvider {
     | { shouldAllow: true }
     | { shouldAllow: false; blockReason: Optional<string> }
   > {
-    const isSanctioned = await this.isSanctionedAddress(fromAddressLowercase);
-    if (isSanctioned) {
-      return {
-        shouldAllow: false,
-        blockReason: 'Address is sanctioned',
-      };
-    }
-
     if (TEST_BAD_ADDRESS_LIST.includes(fromAddressLowercase)) {
       return { shouldAllow: false, blockReason: 'Test address is blocked' };
     }
 
     return { shouldAllow: true };
   }
-
-  private isSanctionedAddress = async (address: string): Promise<boolean> => {
-    try {
-      const apiKey = process.env.CHAINALYSIS_API_KEY;
-      if (!isDefined(apiKey)) {
-        throw new Error('No API Key for Chainalysis OFAC API');
-      }
-
-      const url = `https://public.chainalysis.com/api/v1/address/${address}`;
-
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      const { data } = await axios.get(url, {
-        headers: { 'X-API-KEY': apiKey },
-      });
-
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      const identifications = data.identifications as {
-        category: string;
-        source: string;
-      }[];
-
-      const isSanctioned =
-        isDefined(identifications) &&
-        identifications.some(id => id.category === 'sanctions');
-
-      return isSanctioned;
-    } catch (err) {
-      if (!(err instanceof Error)) {
-        throw err;
-      }
-      throw new Error(`Could not screen address: ${err.message}.`);
-    }
-  };
 }
