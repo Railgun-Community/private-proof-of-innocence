409dee22e3f69dbf2009f9f9e28f51642bba1d2b
ptsimpso <ptaylorsimpson@gmail.com>
2024-02-05 13:47:37 -0800

    Removes test code

--- packages/node/src/local-list-provider.ts
+++ packages/node/src/local-list-provider.ts
@@ -1,6 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import { NetworkName, isDefined, TXIDVersion } from '@railgun-community/shared-models';
+import {
+  NetworkName,
+  isDefined,
+  TXIDVersion,
+} from '@railgun-community/shared-models';
 import {
   ListProvider,
   ListProviderConfig,
@@ -28,10 +32,6 @@ export class LocalListProvider extends ListProvider {
         shouldAllow: false,
         blockReason: 'Address is sanctioned',
       };
-    }
-
-    if (fromAddressLowercase === '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266') {
-      return { shouldAllow: false, blockReason: 'Test address is blocked' };
     }
 
     return { shouldAllow: true };
